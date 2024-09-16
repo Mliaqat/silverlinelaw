345a72eebd10cfd59dc51dccb14ecd871aa785da
Liaqat <liaqattops@gmail.com>
2024-09-16 15:31:29 +0500

    Add New Changes

--- app/practice-area/page.js
+++ app/practice-area/page.js
@@ -1,6 +1,10 @@
 import PracticeArea from "@/section/Pages/PracticePage/PracticePage";
 import React from "react";
-
+export const metadata = {
+  title: "Practice  Area",
+  description:
+    "In today’s increasingly regulated markets, it is more crucial than ever to establish commercial agreements that maximize the benefits of you",
+};
 function page() {
   return <PracticeArea />;
 }
